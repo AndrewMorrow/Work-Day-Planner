a5da964a7ce35ce218fe51b9bbe025d9713dcd9f
Andrew Morrow <amorrow1991@gmail.com>
2020-10-07 01:31:42 -0500

    targeting and appending timeblock-container

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const currentDay = $("#currentDay");
+const timeBlockContainer = $(".timeblock-container");
 const m = moment();
 const dateFormat = m.format("dddd, MMMM Do");
-
-// shows current day at top of the page
-currentDay.text(dateFormat);
+var dayStartHour = moment().set({ hour: 9, minutes: 0, seconds: 0 });
+// var hour = moment().get("hour");
 // var day = moment().get("day");
 // var year = moment().get("year");
-// var hour = moment().get("hour");
 // var date = moment().get("date");
+// console.log(dayStartHour);
+// shows current day at top of the page
+console.log(dayStartHour);
+currentDay.text(dateFormat);
+
+var hourSection = $("<p>" + dayStartHour + "</p>");
+timeBlockContainer.append(hourSection);
 
 // this is how our date should appear
-console.log(m.format("dddd, MMMM Do"));
+// console.log(m.format("dddd, MMMM Do"));
