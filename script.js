3d205594a5194b23851ad6f0c9adda29a3b16f84
AndrewMorrow <beardedmongo@gmail.com>
2020-10-09 18:56:00 -0700

    adds the local storage functionality

--- script.js
+++ script.js
@@ -2,35 +2,27 @@ const currentDay = $("#currentDay");
 const timeBlockContainer = $(".timeblock-container");
 const m = moment();
 const dateFormat = m.format("dddd, MMMM Do");
-// var dayStartHour = moment(9, "hours").format("hA");
 var dayStartHour = m.set("h", 9).set("m", 0).set("s", 0);
 var workHours = 8;
 var textSection;
-// var hour = moment().get("hour");
-// var day = moment().get("day");
-// var year = moment().get("year");
-// var date = moment().get("date");
-// console.log(dayStartHour);
-// shows current day at top of the page
-// console.log(dayStartHour);
 currentDay.text(dateFormat);
-// console.log(moment().get("h"));
-// console.log(dayStartHour.hour());
-// console.log(dayStartHour.hour() < moment().get("h"));
+
 for (let i = 0; i <= workHours; i++) {
-    // nextHour = m.add(1, "hour").format("hA");
-
     setTimeBlock();
-    dayStartHour.add(1, "h");
     setEventValues();
+    dayStartHour.add(1, "h");
 }
-
 function setTimeBlock() {
     var row = $("<div class= 'row time-block'>");
     var hourSection = $(
         "<div class = 'col-md-1 hour'>" + dayStartHour.format("hA") + "</div>"
     );
-    var textSection = $("<textarea class = 'col-md-10 description'>");
+
+    var textSection = $(
+        `<textarea class = 'col-md-10 description' data-time="${dayStartHour.format(
+            "hA"
+        )}"> `
+    );
     var saveBtn = $("<button class= 'col-md-1 btn saveBtn'>");
     var saveIcon = $("<i class = 'fas fa-save'>");
 
@@ -48,24 +40,23 @@ function setTimeBlock() {
     $(saveBtn).append(saveIcon);
 }
 
-$("button").on("click", saveEvent);
+$("i").on("click", saveEvent);
 function saveEvent(e) {
     e.preventDefault();
-    console.log("clicked");
-    var descriptionTextArea = $("textarea");
-    newEvent = descriptionTextArea.val().trim();
-    if (newEvent === "") {
-        console.log("oops");
-        console.log(newEvent);
-        return;
-    }
-    console.log(newEvent);
-    localStorage.setItem("Event", newEvent);
+    var storageName = e.target.parentNode.previousElementSibling.value; /**/
+    console.log(storageName);
+    var storageValue = e.target.parentNode.previousElementSibling.dataset.time;
+    console.log(storageValue);
+    localStorage.setItem(storageValue, storageName);
 }
 
 function setEventValues() {
-    // newEvent = $("textarea").value;
-    newEvent = localStorage.getItem("Event") || "";
+    for (j = 0; j < document.querySelectorAll(" .row > textarea").length; j++) {
+        var searchItem = document.querySelectorAll(
+            "body > div > div > textarea"
+        )[j].dataset.time;
+        document.querySelectorAll("body > div > div > textarea")[
+            j
+        ].value = localStorage.getItem(searchItem);
+    }
 }
-// this is how our date should appear
-// console.log(m.format("dddd, MMMM Do"));
